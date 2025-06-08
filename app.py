218402bdea224c6bee42cfd96822d32d7cfee217
Tom E Harnes <protoxcode@gmail.com>
2025-06-08 11:13:13 +0200

    Fallback image
    Added fallback image for profile picture if missing.
    Added configuration file with auto creation.
    
    Took 38 minutes

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
+import os
+
+import yaml
 from dash import Dash, html, Output, Input, dcc
 
 from api_client.mock_client import MockERPClient
 from logger import logger
 
 APP_TITLE = 'OnSite Presence Monitor'
-UPDATE_INTERVAL = 30_000
-IMAGE_DIRECTORY = 'assets/employee_images/'
+
+DEFAULT_CONFIG = {
+    'location': 'Factory',
+    'update_interval': 30000,
+    'image_directory': 'assets/employee_images/',
+    'erp_api_url': 'http://localhost:8000/api',
+    'erp_api_key': '',
+    'erp_api_client': '',
+    'erp_api_secret': '',
+    'db_type': '',
+    'db_host': '',
+    'db_port': 5432,
+    'db_name': 'onsite',
+    'db_user': 'user',
+    'db_password': 'password',
+    'jwt_secret': '',
+    'jwt_algo': 'HS256'
+}
+
+
+def load_config(path='config.yaml') -> dict:
+    """ Loads local config file, creates a default if the file is missing. """
+    if not os.path.exists(path):
+        logger.info(f'No config file found, creating default at {path}')
+        with open(path, mode='w', encoding='utf-8') as f:
+            yaml.dump(DEFAULT_CONFIG, f)
+        return DEFAULT_CONFIG
+    with open(path, mode='r', encoding='utf-8') as f:
+        return yaml.safe_load(f)
+
+
+CONFIG = load_config()
+UPDATE_INTERVAL = CONFIG['update_interval']
+IMAGE_DIRECTORY = CONFIG['image_directory']
+LOCATION = CONFIG['location']
 
 erp_client = MockERPClient()
 app = Dash(title=APP_TITLE)
@@ -14,22 +50,30 @@ app.layout = html.Div([
     html.H2(APP_TITLE),
     dcc.Interval(id='update-interval', interval=UPDATE_INTERVAL,
                  n_intervals=0),
-    # every 30 seconds
     html.Div(id='worker-container', className='dashboard-container')
 ])
 
 
 def render_workers() -> list or html:
-    active_workers = [w for w in erp_client.get_workers() if w.status]
+    active_workers = [w for w in erp_client.get_workers() if
+                      w.status and w.location == LOCATION]
     logger.info(f"Active workers updated: {len(active_workers)}")
 
     if not active_workers:
         return html.Div("No one is currently clocked in.",
                         className='empty-message')
 
+    def get_image_path(worker_id: int):
+        path = f'{IMAGE_DIRECTORY}{worker_id}.png'
+        if not os.path.exists(path):
+            return 'assets/default.png'
+        return path
+
     return [
         html.Div([
-            html.Img(src=f'{IMAGE_DIRECTORY}{worker.id_number}.png'),
+            html.Img(
+                src=get_image_path(worker.id_number),
+            ),
             html.P(worker.name)
         ], className="card")
         for worker in active_workers
